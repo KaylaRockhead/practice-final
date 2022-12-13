7517d640345781e132bcb0defc686119d6161839
KaylaRockhead <112909781+KaylaRockhead@users.noreply.github.com>
2022-12-13 10:59:47 -0500

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -7,9 +7,15 @@ import { CartService } from '../cart.service';
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent  implements OnInit {
 
   product: Product | undefined;
+  
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  } 
+
   constructor(private route: ActivatedRoute,
     private cartService: CartService
     
