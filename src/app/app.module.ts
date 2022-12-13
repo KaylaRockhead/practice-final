97f08b89f8539d0a2a36307ed1e9305ea59c8dca
KaylaRockhead <112909781+KaylaRockhead@users.noreply.github.com>
2022-12-13 11:25:36 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { ShippingComponent } from './shipping/shipping.component';
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
+
     ])
   ],
   declarations: [
