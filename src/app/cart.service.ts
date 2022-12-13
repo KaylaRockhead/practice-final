b954256ea7cc4ad71d09f48a5f8098825e604ed3
KaylaRockhead <112909781+KaylaRockhead@users.noreply.github.com>
2022-12-13 11:19:38 -0500

    2 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from './products';
 @Injectable({
@@ -5,6 +6,8 @@ import { Product } from './products';
 })
 export class CartService {
   items: Product[] = [];
+
+  
 /* . . . */
 
   addToCart(product: Product) {
@@ -19,5 +22,15 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
+
+
+  constructor(
+    
+    private http: HttpClient
+  ) {}
+
 /* . . . */
 }
